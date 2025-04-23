4b7cbd5ddea778072fc353bdedce790cbf223939
Lovenson <beaucicotlovenson@gmail.com>
2025-04-23 16:44:06 +0800

    feat: Added EmojiPicker component

--- components/Button.tsx
+++ components/Button.tsx
@@ -36,10 +36,7 @@ export default function Button({ label, theme, onPress }: ButtonProps) {
 
   return (
     <View style={styles.buttonContainer}>
-      <Pressable
-        style={styles.button}
-        onPress={() => alert(`You pressed button ${label}`)}
-      >
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
